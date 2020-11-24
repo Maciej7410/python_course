a844e43c109bb87750c65d270cf32b4609050398
Maciej Nowak <m_wlodek@poczta.onet.pl>
2020-11-24 06:15:15 +0100

    Pierwszy dzień nauki

---
+++ Python_course/01_cwiczenia.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+print('Ucz się Pythona!')
+
+age = 20
+print(f'Mam {age} lat')
+
+# %%
+language = 'Python'
+python_version = '3.8'
+print (f'Uczę się języka {language} w wersji {python_version}')
